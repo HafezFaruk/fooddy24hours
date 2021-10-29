ff48d0ce7fddd10956b0071080a8c5185fadc866
HafezFaruk <hmomarfaruk1454@gmail.com>
2021-10-29 23:42:12 +0600

    update user

--- src/Pages/UpdateUser/UpdateUser.js
+++ src/Pages/UpdateUser/UpdateUser.js
@@ -1,45 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import axios from 'axios';
-import { useForm } from "react-hook-form";
 
 const UpdateUser = () => {
-    const [service,setService] = useState({});
-    const {name,price,img,description} = service;
-    const {id} = useParams();
-    const { register, handleSubmit, reset } = useForm();
-    const onSubmit = data => {
-        axios.post('http://localhost:5000/services',data)
-        .then(res => {
-            console.log(res);
-            if (res.data.insertedId) {
-                alert('Update successfully');
-                reset();
-            }
-        })
+    const { id } = useParams();
+    const [service, setService] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${id}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
+    // console.log(service)
+
+
+    const handleName = e => {
+        const name = e.target.value;
+        const updateName = { ...service, name: name };
+        setService(updateName)
     }
 
-    useEffect(()=>{
+    const handleDescription = e => {
+        const description = e.target.value;
+        const updateDescription = { ...service, description };
+        setService(updateDescription)
+    }
+    const handlePrice = e => {
+        const price = e.target.value;
+        const updatePrice = { ...service, price };
+        setService(updatePrice)
+        // console.log(updatePrice)
+    }
+
+    const handleUrl = e => {
+        const url = e.target.value;
+        const updateUrl = { ...service, img: url };
+        setService(updateUrl)
+        console.log(updateUrl)
+    }
+    const handleUpdate = e => {
         const url = `http://localhost:5000/services/${id}`;
-        fetch(url)
+        fetch(url, {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(service)
+        })
             .then(res => res.json())
-            .then(data => setService(data));
-    },[]);
-    const handleNameChange = e => {
-        const updateName = e.target.value;
-        const updatUser = {...service};
-        setService(updateName);
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert("successfully")
+                    setService({})
+                    e.target.reset()
+                }
+            })
+        e.preventDefault()
     }
     return (
         <div>
-            <h1>Update add a Service</h1>
-            <p>{id}</p>
-            <form className="w-50 mx-auto mt-3" onSubmit={handleSubmit(onSubmit)}>
-                <input onChange={handleNameChange} value={name || ''} placeholder="name" className="w-100 mt-2 py-2" {...register("name", { required: true, maxLength: 20 })} />
-                <textarea value={description || ''} placeholder="description" className="w-100 mt-2 py-2" {...register("description")} />
-                <input value={price || ''} placeholder="price" className="w-100 mt-2 py-2" type="number" {...register("price")} />
-                <input value={img || ''} placeholder="img_url" className="w-100 mt-2 py-2" {...register("img")} />
-                <input className="fs-4 w-100 mt-2 py-2 btn btn-success" type="submit" value="Submit"/>
+            <h4>update user : {id} </h4>
+            <form className="w-50 mx-auto" onSubmit={handleUpdate}>
+                <input className="w-100 mt-2 py-2" onChange={handleName} value={service?.name || ''} type="text" placeholder="name" />
+                <textarea className="w-100 mt-2 py-2" onChange={handleDescription} type="text" value={service.description || ''} cols="6" rows="4" placeholder="description"
+                ></textarea>
+                <input className="w-100 mt-2 py-2" onChange={handlePrice} value={service.price || ''} type="number" placeholder="price" />
+                <input className="w-100 mt-2 py-2" onChange={handleUrl} value={service?.img || ''} type="text" placeholder="img url" />
+                <input className="w-100 mt-2 py-2 btn btn-success" type="submit" value="Update" />
             </form>
         </div>
     );
